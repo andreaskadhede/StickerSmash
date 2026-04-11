a53b868a09c1993220cce0fc080be27a1e1788a1
andreaskadhede <andreaskadhede@icloud.com>
2026-04-11 08:56:12 +0200

    Take a screenshot
    Add image picking and saving functionality with media library support

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,18 +1,21 @@
-import Button from '@/components/Button';
-import ImageViewer from '@/components/ImageViewer';
+import * as ImagePicker from 'expo-image-picker';
+import * as MediaLibrary from 'expo-media-library';
+import { useEffect, useRef, useState } from 'react';
 import { ImageSourcePropType, StyleSheet, View } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { captureRef } from 'react-native-view-shot';
 
-import * as ImagePicker from 'expo-image-picker';
-import { useState } from 'react';
-
+import Button from '@/components/Button';
 import CircleButton from '@/components/CircleButton';
 import EmojiList from '@/components/EmojiList';
 import EmojiPicker from '@/components/EmojiPicker';
-import EmojiSticker from '@/components/EmojiSticker';
 import IconButton from '@/components/IconButton';
+import ImageViewer from '@/components/ImageViewer';
 
-const placeholderImg = require('@/assets/images/background-image.png');
+import EmojiSticker from '@/components/EmojiSticker';
+
+const PlaceholderImage = require('@/assets/images/background-image.png');
+
 export default function Index() {
 	const [selectedImage, setSelectedImage] = useState<string | undefined>(
 		undefined,
@@ -22,6 +25,14 @@ export default function Index() {
 	const [pickedEmoji, setPickedEmoji] = useState<
 		ImageSourcePropType | undefined
 	>(undefined);
+	const [permissionResponse, requestPermission] = MediaLibrary.usePermissions();
+	const imageRef = useRef<View>(null);
+
+	useEffect(() => {
+		if (!permissionResponse?.granted) {
+			requestPermission();
+		}
+	}, []);
 
 	const pickImageAsync = async () => {
 		let result = await ImagePicker.launchImageLibraryAsync({
@@ -51,16 +62,33 @@ export default function Index() {
 	};
 
 	const onSaveImageAsync = async () => {
-		// we will implement this later
+		try {
+			const localUri = await captureRef(imageRef, {
+				height: 440,
+				quality: 1,
+			});
+
+			await MediaLibrary.saveToLibraryAsync(localUri);
+			if (localUri) {
+				alert('Saved!');
+			}
+		} catch (e) {
+			console.log(e);
+		}
 	};
 
 	return (
 		<GestureHandlerRootView style={styles.container}>
 			<View style={styles.imageContainer}>
-				<ImageViewer source={placeholderImg} selectedImage={selectedImage} />
-				{pickedEmoji && (
-					<EmojiSticker imageSize={40} stickerSource={pickedEmoji} />
-				)}
+				<View ref={imageRef} collapsable={false}>
+					<ImageViewer
+						source={PlaceholderImage}
+						selectedImage={selectedImage}
+					/>
+					{pickedEmoji && (
+						<EmojiSticker imageSize={40} stickerSource={pickedEmoji} />
+					)}
+				</View>
 			</View>
 			{showAppOptions ? (
 				<View style={styles.optionsContainer}>
@@ -77,8 +105,8 @@ export default function Index() {
 			) : (
 				<View style={styles.buttonContainer}>
 					<Button
-						label='Choose a photo'
 						variant='primary'
+						label='Choose a photo'
 						onPress={pickImageAsync}
 					/>
 					<Button
